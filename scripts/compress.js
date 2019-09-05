203ba1f7dbe99f0d5b96a4ce984b235c3b12a462
wuwei <wuwei@bytedance.com>
2019-09-05 16:06:40 +0800

    fix compress

--- scripts/compress.js
+++ scripts/compress.js
@@ -6,6 +6,7 @@ const name = require('../build/manifest.json').name;
 const argv = require('minimist')(process.argv.slice(2));
 
 const keyPath = argv.key || 'key.pem';
+argv.codebase = argv.codebase || 'http://localhost:8000/beijing-charity.crx';
 const existsKey = fs.existsSync(keyPath);
 const crx = new ChromeExtension({
   appId: argv['app-id'],
@@ -16,13 +17,13 @@ const crx = new ChromeExtension({
 crx.load(path.resolve('build'))
   .then(() => crx.pack())
   .then((archiveBuffer) => {
-    fs.writeFile(`${name}.zip`, archiveBuffer);
+    fs.writeFileSync(`${name}.zip`, archiveBuffer);
 
     if (!argv.codebase || !existsKey) return;
     crx.pack(archiveBuffer).then((crxBuffer) => {
       const updateXML = crx.generateUpdateXML();
 
-      fs.writeFile('update.xml', updateXML);
-      fs.writeFile(`${name}.crx`, crxBuffer);
+      fs.writeFileSync('update.xml', updateXML);
+      fs.writeFileSync(`${name}.crx`, crxBuffer);
     });
   });
